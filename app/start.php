8e963e3306b21f9a07b426e090216194f6467ffd
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-05-21 02:05:16 +0700

    covid-19 api library

--- app/start.php
+++ app/start.php
@@ -17,9 +17,34 @@ $tpl = new TemplatePower("../template/_tp_main.html");
 $tpl->assignInclude("body", "../template/_tp_index.html");
 $tpl->prepare();
 
-
 $tpl->assign("_ROOT.Header","FuFu Dev");
-$tpl->assign("_ROOT.Description","วันที่ ".$strDate."<br>พัฒนาขึ้นเพื่อใช้ในการทำงานภายใต้ VPS Live เท่านั้น อัพเดทล่าสุด");
+$tpl->assign("_ROOT.Description","วันที่ ".$strDate.
+"<br>พัฒนาขึ้นเพื่อใช้ในการทำงานภายใต้ VPS Live เท่านั้น อัพเดทล่าสุด<br>");
+
+
+// DATA from API //////////////////////////
+$Confirmed        = $covid19_th->Confirmed;
+$Recovered        = $covid19_th->Recovered;
+$Hospitalized     = $covid19_th->Hospitalized;
+$Deaths           = $covid19_th->Deaths;
+$NewConfirmed     = $covid19_th->NewConfirmed;
+$NewRecovered     = $covid19_th->NewRecovered;
+$NewHospitalized  = $covid19_th->NewHospitalized;
+$NewDeaths        = $covid19_th->NewDeaths;
+$UpdateDate        = $covid19_th->UpdateDate;
+
+
+///////////////////////////////////////////
+$tpl->newBlock("DAYLIREPORT_COVID19TH");
+$tpl->assign("Covid19th_Confirmed", number_format($Confirmed));
+$tpl->assign("Covid19th_Recovered", number_format($Recovered));
+$tpl->assign("Covid19th_NewRecovered", number_format($NewRecovered));
+$tpl->assign("Covid19th_Hospitalized", number_format($Hospitalized));
+$tpl->assign("Covid19th_Deaths", number_format($Deaths));
+$tpl->assign("Covid19th_NewDeaths", number_format($NewDeaths));
+$tpl->assign("Covid19th_NewConfirmed", number_format($NewConfirmed));
+$tpl->assign("Covid19th_UpdateDate", $UpdateDate);
+
 
 
 //FRONTPAGESEO("1",$lag);
--- include/config.inc.php
+++ include/config.inc.php
@@ -9,6 +9,19 @@ $strDate = date("Y-m-d H:i:s");
 
 // API Base Master
 
+
+
+
+// COVID19
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://covid19.th-stat.com/api/open/today");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($curl);
+$covid19_th = json_decode($output);
+curl_close($curl);
+
+
+
 // Bank Connection
 
 ?>
